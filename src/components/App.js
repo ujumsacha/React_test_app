ffc396eebfa574c09e6dd98e7a8a4440d09445ea
ujumsacha <maxime.ankon@gmail.com>
2022-10-17 20:01:33 +0000

    second commit

--- src/components/App.js
+++ src/components/App.js
@@ -2,12 +2,14 @@ import logo from '../assets/logo.svg';
 import '../App.css';
 import Banner from './Banner';
 import Cart from './Cart';
+import ShoppingList from './shoppingList';
 
 function App() {
   return (
     <div>
       <Banner/>
       <Cart/>
+      <ShoppingList/>
     </div>
   );
 }
